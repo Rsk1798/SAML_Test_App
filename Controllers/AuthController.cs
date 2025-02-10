eec5d05234453765ce72330ce56ebbf6d1d513a9
CrazyStuff <crazy.k.stuff@gmail.com>
2025-02-11 02:16:50 +0530

    Chanes configured according to deployement

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -42,7 +42,8 @@ namespace SAML_Test_App.Controllers
             }
             binding.Unbind(Request.ToGenericHttpRequest(), saml2AuthnResponse);
             await saml2AuthnResponse.CreateSession(HttpContext);
-            return Redirect("https://localhost:44348");
+            // return Redirect("https://localhost:44348");
+            return Redirect("https://samltestapp-production.up.railway.app");
         }
 
         public IActionResult Index()
